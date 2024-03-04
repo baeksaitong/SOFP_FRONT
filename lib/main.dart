bea12b25a3d3c7ed80fc0980d9266b78ee1c2849
Hanjo <144607552+Hanjo35@users.noreply.github.com>
2024-03-04 20:40:29 +0900

    마이페이지
    마이페이지 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sofp_front/gaps.dart';
 
+import 'mypage.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MyPage());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,16 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Row(
-            children: [
-              Text('data'),
-              Gaps.w20, // 여백을 부여할 수 있습니다.
-              IconButton(
+            title: Row(
+              children: [
+                Text('data'),
+                Gaps.w20, // 여백을 부여할 수 있습니다.
+                IconButton(
                   onPressed: () {},
                   icon: Icon(Icons.search),
-              )
-            ],
-          )
+                )
+              ],
+            )
         ),
       ),
     );
---
+++ lib/mypage.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:sofp_front/gaps.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: null,
+        body: Stack(
+          children: [
+            Positioned(
+              top:90,
+              left:50,
+              child: Text(
+                '???님\n안녕하세요!',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+              ),
+            ),
+            Positioned(
+              top: 170,
+              left: 60,
+              child: ElevatedButton(
+                onPressed: () {
+                  // 정보 수정 버튼 클릭 시 동작할 내용
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  elevation: 0,
+                  padding: EdgeInsets.zero,
+                ),
+                child: Text('정보 수정'),
+              ),
+            ),
+            Positioned(
+              top: 90,
+              left: 270,
+              child: InkWell(
+                onTap: () {
+
+                },
+                child: Container(
+                  width: 75,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.greenAccent, width: 1),
+                    color: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: AssetImage('free-icon-user-icon-4360835.png'),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 240,
+              left: 35,
+                child: Container(
+                  width: 330,
+                  height: 215,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: 330,
+                          height: 215,
+                          decoration: ShapeDecoration(
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Color(0xFF53DACA)),
+                              borderRadius: BorderRadius.circular(35),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 20,
+                        top: 15,
+                        child: SizedBox(
+                          width: 200.21,
+                          height: 33.04,
+                          child: Text(
+                            '내 알레르기 & 질병',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 270,
+                        top: 15,
+                        child: SizedBox(
+                          width: 30,
+                          height: 20,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // 정보 수정 버튼 클릭 시 동작할 내용
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.transparent,
+                              elevation: 0,
+                              padding: EdgeInsets.zero,
+                            ),
+                            child: Text('수정'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ),
+            Positioned(
+              top: 490,
+              left: 35,
+              child: Container(
+                width: 330,
+                height: 215,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 330,
+                        height: 215,
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1, color: Color(0xFF53DACA)),
+                            borderRadius: BorderRadius.circular(35),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 20,
+                      top: 15,
+                      child: SizedBox(
+                        width: 200.21,
+                        height: 33.04,
+                        child: Text(
+                          '내가 복용 중인 약',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 270,
+                      top: 15,
+                      child: SizedBox(
+                        width: 30,
+                        height: 20,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // 정보 수정 버튼 클릭 시 동작할 내용
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.transparent,
+                            elevation: 0,
+                            padding: EdgeInsets.zero,
+                          ),
+                          child: Text('수정'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
