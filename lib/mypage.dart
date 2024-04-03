ec301b98b0e2df18cc8fb7d5fc64f204d6e6e230
Hanjo <144607552+Hanjo35@users.noreply.github.com>
2024-04-04 04:30:35 +0900

    #4 마이페이지 UI 동적으로 구현

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -10,11 +10,17 @@ class MyPagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: IconButton(
+          onPressed: () => popCurrentScreen(context),
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
       body: MyPage(),
     );
   }
 }
-
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -28,27 +34,43 @@ class _MyPageState extends State<MyPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: IconButton(
-          onPressed: () => popCurrentScreen(context),
-          icon: Icon(Icons.arrow_back),
-        ),
-      ),
-      body: Stack(
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Positioned(
-            top: 90,
-            left: 20,
-            child: Text(
-              '$str 님\n안녕하세요!',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
-            ),
+          SizedBox(height: 60),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    '$str 님\n안녕하세요!',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.greenAccent, width: 1),
+                    color: Colors.white,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    child: Image.asset('assets/free-icon-user-icon-4360835.png', width: 70, height: 70),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            top: 160,
-            left: 20,
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 30),
             child: ElevatedButton(
               onPressed: () {
                 // 정보 수정 버튼 클릭 시 동작할 내용
@@ -61,153 +83,71 @@ class _MyPageState extends State<MyPage> {
               child: Text('정보 수정'),
             ),
           ),
-          Positioned(
-            top: 80,
-            left: 220,
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 20), // 상단 간격만 유지하고, 좌우 패딩 제거
             child: Container(
-              width: 100,
-              height: 100,
               decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.greenAccent, width: 1),
                 color: Colors.white,
+                border: Border.all(color: Color(0xFF53DACA), width: 1),
+                borderRadius: BorderRadius.circular(35),
               ),
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                child: Image.asset(
-                  'assets/free-icon-user-icon-4360835.png',
-                  width: 70, // 여기서 이미지의 실제 크기를 조절
-                  height: 70, // 여기서 이미지의 실제 크기를 조절
-                  fit: BoxFit.fill, // 이미지가 할당된 공간을 꽉 채우도록 조절,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 185, top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '내 알레르기 및 질병',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(height: 80), // 상자 아래로 늘리기
+                    ElevatedButton(
+                      onPressed: () {
+                        // 수정 버튼 클릭 시 동작
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Text('수정', style: TextStyle(fontSize: 16)),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          Positioned(
-            top: 220,
-            left: 15,
-            child: SizedBox(
-              width: 330,
-              height: 215,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      width: 330,
-                      height: 180,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFF53DACA)),
-                          borderRadius: BorderRadius.circular(35),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 20,
-                    top: 15,
-                    child: SizedBox(
-                      width: 200.21,
-                      height: 33.04,
-                      child: Text(
-                        '내 알레르기 & 질병',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w400,
-                          height: 0,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 270,
-                    top: 15,
-                    child: SizedBox(
-                      width: 30,
-                      height: 20,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // 정보 수정 버튼 클릭 시 동작할 내용
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                          padding: EdgeInsets.zero,
-                        ),
-                        child: Text('수정'),
-                      ),
-                    ),
-                  ),
-                ],
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 20), // 상단 간격만 유지 하고, 좌우 패딩 제거
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color(0xFF53DACA), width: 1),
+                borderRadius: BorderRadius.circular(35),
               ),
-            ),
-          ),
-          Positioned(
-            top: 420,
-            left: 15,
-            child: SizedBox(
-              width: 330,
-              height: 210,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      width: 330,
-                      height: 180,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFF53DACA)),
-                          borderRadius: BorderRadius.circular(35),
-                        ),
-                      ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 205, top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '내가 복용 중인 약',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
                     ),
-                  ),
-                  Positioned(
-                    left: 20,
-                    top: 15,
-                    child: SizedBox(
-                      width: 200.21,
-                      height: 33.04,
-                      child: Text(
-                        '내가 복용 중인 약',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w400,
-                          height: 0,
-                        ),
+                    SizedBox(height: 80), // 상자 아래로 늘리기
+                    ElevatedButton(
+                      onPressed: () {
+                        // 수정 버튼 클릭 시 동작
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                        padding: EdgeInsets.zero,
                       ),
+                      child: Text('수정', style: TextStyle(fontSize: 16)),
                     ),
-                  ),
-                  Positioned(
-                    left: 270,
-                    top: 15,
-                    child: SizedBox(
-                      width: 30,
-                      height: 20,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // 정보 수정 버튼 클릭 시 동작할 내용
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                          padding: EdgeInsets.zero,
-                        ),
-                        child: Text('수정'),
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
