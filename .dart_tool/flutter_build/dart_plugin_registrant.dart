fb716cca84c10fe2a3942b17e3a4169481c20533
Seong-Myeong <mg0454@naver.com>
2024-05-25 22:20:22 +0900

    Feat: 구글 맵 지도 추가 (#73)

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -7,9 +7,11 @@
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:camera_android/camera_android.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
 import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:camera_avfoundation/camera_avfoundation.dart';
+import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:file_selector_linux/file_selector_linux.dart';
@@ -40,6 +42,15 @@ class _PluginRegistrant {
       }
 
       try {
+        GoogleMapsFlutterAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         ImagePickerAndroid.registerWith();
       } catch (err) {
         print(
@@ -63,6 +74,15 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`camera_avfoundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        GoogleMapsFlutterIOS.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_ios` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
