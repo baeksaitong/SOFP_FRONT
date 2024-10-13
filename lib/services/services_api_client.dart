1707a2e7d3e35a4dae55ef23050d894ff96903e5
Seong-Myeong <mg0454@naver.com>
2024-10-13 22:50:32 +0900

    Feat: CI/CD 구축

--- lib/services/services_api_client.dart
+++ lib/services/services_api_client.dart
@@ -4,11 +4,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sopf_front/managers/managers_jwt.dart';
 
 class APIClient {
-  static String? baseUrl = dotenv.env['API_URL'];
+  static String baseUrl = const String.fromEnvironment('API_URL', defaultValue: 'http://default-url.com');
   final JWTManager _jwtManager = JWTManager();
 
   Uri buildUri(String endpoint) {
-    // baseUrl과 endpoint를 결합하여 완전한 URL을 만듭니다.
+    if (baseUrl.isEmpty) {
+      throw ArgumentError('Base URL not set');
+    }
+    // Ensure the endpoint is correctly formatted
     return Uri.parse('$baseUrl$endpoint');
   }
   Future<String> getValidAccessToken() async {
