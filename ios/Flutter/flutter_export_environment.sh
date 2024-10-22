4544e32f504397b0e02549b70f4507fe9ec4a1a4
Seong-Myeong <mg0454@naver.com>
2024-10-22 19:27:09 +0900

    Feat: 머지 후 파일 처리

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mingmaengmung/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mingmaengmung/Documents/GitHub/SOFP_FRONT"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/mingmaengmung/Documents/GitHub/SOFP_FRONT/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=QVBJX1VSTD1odHRwOi8vMy4zOS44LjE0Nzo4MDgw,R09PR0xFX01BUF9BUElfS0VZPUFJemFTeUJ2azNxcXVRZU9VQjVzRWRqZ2J4MFF2cDIxN2ZIMkZYUlNr,TkFWRVJfQ0xJRU5UX0lEPXNfc1U4N3FVZmFIVG9TaV9reThS,Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/mingmaengmung/Documents/GitHub/SOFP_FRONT/.dart_tool/package_config.json"
